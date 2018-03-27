600b4b27ac3e50e91ec50c04e3c2d49455ef3d27
simonerl <simonerl@kth.se>
2018-03-27 10:14:12 +0200

    Bugfix

--- motorstyrning.py
+++ motorstyrning.py
@@ -6,54 +6,57 @@
 ###################################################
 
 
+class Hbrygga:
+    def __init__():
+        pass
+    def setup():
+        import RPi.GPIO as GPIO
+        import time
+        
+        #setup GPIO using Board numbering
+        GPIO.setmode(GPIO.BOARD)
+        
+        #Controller GPIO-pins: 31 33 35 37
+        #Enabling pins: 36 38
 
-def setup():
-    import RPi.GPIO as GPIO
-    import time
-    
-    #setup GPIO using Board numbering
-    GPIO.setmode(GPIO.BOARD)
-    
-    #Controller GPIO-pins: 31 33 35 37
-    #Enabling pins: 36 38
+        #Setting up outputs:
+        self.ctrlpins_list = [31,33,35,37]
+        self.enblpins_list = [36,38]
+        GPIO.setup(ctrlpins_list, GPIO.OUT)
+        GPIO.setup(enblpins_list, GPIO.OUT, initial=GPIO.HIGH)
 
-    #Setting up outputs:
-    self.ctrlpins_list = [31,33,35,37]
-    self.enblpins_list = [36,38]
-    GPIO.setup(ctrlpins_list, GPIO.OUT)
-    GPIO.setup(enblpins_list, GPIO.OUT, initial=GPIO.HIGH)
-
-    self.state0=(GPIO.LOW, GPIO.LOW, GPIO.LOW, GPIO.LOW) #Idle
-    self.state1=(GPIO.HIGH, GPIO.LOW, GPIO.HIGH, GPIO.LOW)
-    self.state2=(GPIO.HIGH, GPIO.LOW, GPIO.LOW, GPIO.HIGH)
-    self.state3=(GPIO.LOW, GPIO.HIGH,GPIO.LOW, GPIO.HIGH)
-    self.state4=(GPIO.LOW, GPIO.HIGH,GPIO.HIGH, GPIO.LOW)
-def loop():
-    #GPIO.output(self.ctrlpins_list, GPIO.LOW)                # sets all to GPIO.LOW
-    #GPIO.output(chan_list, (GPIO.HIGH, GPIO.LOW))   # sets first HIGH and second LOW
-    while True:
-        stepForward(1,0.1)
-def stepForward(steps, s_delay):
-    #Steps: antalet fyra-steg
-    #ms_delay: Hur många sekunder mellan varje steg
-    for i in range(steps):
-        GPIO.output(self.ctrlpins_list, self.state1)
-        time.sleep(s_delay)
-        GPIO.output(self.ctrlpins_list, self.state2)
-        time.sleep(s_delay)
-        GPIO.output(self.ctrlpins_list, self.state3)
-        time.sleep(s_delay)
-        GPIO.output(self.ctrlpins_list, self.state4)
-        time.sleep(s_delay)
-    pass
-def nextState():
-    pass
-def stepBackward(steps, s_delay):
-    pass
-def prevousState():
-    pass
+        self.state0=(GPIO.LOW, GPIO.LOW, GPIO.LOW, GPIO.LOW) #Idle
+        self.state1=(GPIO.HIGH, GPIO.LOW, GPIO.HIGH, GPIO.LOW)
+        self.state2=(GPIO.HIGH, GPIO.LOW, GPIO.LOW, GPIO.HIGH)
+        self.state3=(GPIO.LOW, GPIO.HIGH,GPIO.LOW, GPIO.HIGH)
+        self.state4=(GPIO.LOW, GPIO.HIGH,GPIO.HIGH, GPIO.LOW)
+    def loop():
+        #GPIO.output(self.ctrlpins_list, GPIO.LOW)                # sets all to GPIO.LOW
+        #GPIO.output(chan_list, (GPIO.HIGH, GPIO.LOW))   # sets first HIGH and second LOW
+        while True:
+            stepForward(1,0.1)
+    def stepForward(steps, s_delay):
+        #Steps: antalet fyra-steg
+        #ms_delay: Hur många sekunder mellan varje steg
+        for i in range(steps):
+            GPIO.output(self.ctrlpins_list, self.state1)
+            time.sleep(s_delay)
+            GPIO.output(self.ctrlpins_list, self.state2)
+            time.sleep(s_delay)
+            GPIO.output(self.ctrlpins_list, self.state3)
+            time.sleep(s_delay)
+            GPIO.output(self.ctrlpins_list, self.state4)
+            time.sleep(s_delay)
+        pass
+    def nextState():
+        pass
+    def stepBackward(steps, s_delay):
+        pass
+    def prevousState():
+        pass
 
 if __name__=="__main__":
-    setup()
-    loop()
+    Hb=Hbrygga()
+    Hb.setup()
+    Hb.loop()
     GPIO.cleanup() #Resets the status of any GPIO-pins (run before end)
