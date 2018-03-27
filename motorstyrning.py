f7f39f020707f59d874a7bdfaa26b71211c27214
simonerl <simonerl@kth.se>
2018-03-27 11:13:33 +0200

    test

--- motorstyrning.py
+++ motorstyrning.py
@@ -55,13 +55,13 @@ class Hbrygga:
             time.sleep(s_delay)
 
     def nextState(self):
-        if self.state==3:
+        if self.state==0:
             GPIO.output(self.ctrlpins_list, self.state1)
-        elif self.state==0:
-            GPIO.output(self.ctrlpins_list, self.state2)
         elif self.state==1:
+            GPIO.output(self.ctrlpins_list, self.state2)
+        elif self.state==2:
             GPIO.output(self.ctrlpins_list, self.state3)
-        else: #self.state==4
+        else: #self.state==3
             GPIO.output(self.ctrlpins_list, self.state3)
         self.state=(self.state + 1)%4
     def stepBackward(self, steps, s_delay):
@@ -74,19 +74,11 @@ class Hbrygga:
         
         GPIO.output(self.ctrlpins_list, self.state1)
         time.sleep(speed)
-        GPIO.output(self.ctrlpins_list, self.state0)
-        time.sleep(speed)
         GPIO.output(self.ctrlpins_list, self.state2)
-        time.sleep(speed)
-        GPIO.output(self.ctrlpins_list, self.state0)
         time.sleep(speed)
         GPIO.output(self.ctrlpins_list, self.state3)
         time.sleep(speed)
-        GPIO.output(self.ctrlpins_list, self.state0)
-        time.sleep(speed)
         GPIO.output(self.ctrlpins_list, self.state4)
-        time.sleep(speed)
-        GPIO.output(self.ctrlpins_list, self.state0)
         time.sleep(speed)
         
         self.state=0
