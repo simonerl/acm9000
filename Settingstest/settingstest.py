5428ceec90d5d06c376f3b21a4f9b6fc36ff69b6
simonerl <simonerl@kth.se>
2018-04-06 12:48:36 +0200

    Kommenterad
    Fixad så den kan användas som bilaga i rapporten.

--- Settingstest/settingstest.py
+++ Settingstest/settingstest.py
@@ -1,15 +1,23 @@
+#############################################################
+#   Ett program för att tidtagning av  inställningar        #
+#   för en Raspberry Pi camera v.2 kopplad till en          #
+#   Raspberry Pi 3                                          #
+#   Av: Simon Erlandsson, CMAST                             #
+#   Skapad: 2018-03-26                                      #
+#   Kandidatexamensarbete i Mekatronik 2018                 #
+#############################################################
 import picamera
 import picamera.array
 import time
 
 def restest():
-    """Ska test hur långtid det tar att ta en bild->rgb-matris"""
+    """Testar hur lång tid det tar att ta en bild->rgb-matris"""
     with picamera.PiCamera() as camera:
         with picamera.array.PiRGBArray(camera) as output:
-            ######################################
-            #SETTINGS
-            #Ovissentliga är utkommenterade
-            ###
+            ###################################
+            #       SETTINGS                  #
+            #Ovissentliga är utkommenterade   #
+            ###################################
             #camera.resolution = (width, height)
             #camera.framerate = 10
             #camera_num=0 
@@ -36,20 +44,28 @@ def restest():
             camera.crop = (0.0, 0.0, 1.0, 1.0)
             #Specifikt för camera.capture:
             uvp = False # use_video_port #Use_video_port = True, innebär att bilder tas snabbare som att det vore en video
-            ######################################
+
+            ###########################################################
+            #OBS
+            #Koden måste modifieras för att testa olika inställningar
+            #Inställningsvektorer är hårdkodade och utkommenterade
+            #De kan itereras igenom med t.ex. en for-loop
+            ###########################################################
+
             #sensor_vektor = [0, 1, 2, 3, 4, 5, 6, 7]
             #awb_vektor=['off','auto','sunlight','cloudy','shade','tungsten','fluorescent','incandescent','flash','horizon']
             #meter_vektor=['average','spot','backlit','matrix']
             #exposure_vektor=['off','auto','night','nightpreview','backlight','spotlight','sports','snow','beach','verylong','fixedfps','antishake','fireworks']
             #color_effect_vektor=[None, (128, 128)]
             #tfvektor=[True, False]
-            start=time.time()
-            camera.capture('baest.jpeg') 
-            camera.capture(output, 'rgb',use_video_port=uvp) 
-            #Gör något med output här
             
+            #camera.capture('bild.jpeg') #Kan användas för att testa vilken sorts bild som fås ut
+
+            start=time.time()
+            camera.capture(output, 'rgb',use_video_port=uvp) 
             end=time.time()
             taken=(time.time()-start)
-            print('Att ta bild med bästa settings:'  + str(taken) + ' s')
+            print('Att ta bild med givna sinställningar:'  + str(taken) + ' s')
 
-restest()
+if __name__=="__main__":
+    restest()
