7465b502efdff33fbf1744165a3114d2b868099b
simonerl <simonerl@kth.se>
2018-04-24 11:36:28 +0200

    Printqueue
    Queue som loggar allt som händer och sedan printar den

--- Moduler/main.py
+++ Moduler/main.py
@@ -13,7 +13,7 @@ from imageshooter import *
 from motorstyrning import *
 from imageprocessing import *
 import time
-#import queue
+import queue
 
 #########################################
 #TODO: Logg for images and position etc
@@ -38,6 +38,11 @@ class positionlogg():
         #################
         self.COV = self.steprevolution//4 #Current Center Of View (Position from left border position to center of view)
         self.errorvalue=0 #Where the measured value is compared to the current COV
+
+        #######
+        #OTHER#
+        #######
+        self.textlog=queue.Queue()
         
     def add_steps(self,steps):
         """Adding steps the motor has moved: positive to the right, negative to the left"""
@@ -51,11 +56,11 @@ class positionlogg():
 def init_threaded_modules():
     #New motor module thread 
     motorThread=threading.Thread(target = motor_module, args=(pl,True))
-    motorThread.daemon=False #Will termiate when main-thread ends
+    motorThread.daemon=True #Will termiate when main-thread ends
     motorThread.start()
     #New image module thread 
     imageThread=threading.Thread(target = image_module, args=(pl,))
-    imageThread.daemon=False #Will termiate when main-thread ends
+    imageThread.daemon=True #Will termiate when main-thread ends
     imageThread.start()
     
 ###################################################
@@ -63,43 +68,49 @@ def init_threaded_modules():
 ###################################################
 def motor_module(positionlogg,loop=True):
     """Motor module that runs the motor on a thread"""
+    positionlogg.logtex.enqueue('Initializing motor module')
     H=Hbrygga()
     while loop:
         time.sleep(10)
         PosX = positionlogg.errorvalue
-        print('PosX:',PosX)
+        positionlogg.logtex.enqueue('PosX:'+str(PosX))
         #TODO: add number of steps moved to logg
         if not PosX:
-            print("Im sorry Dave,im afraid i cant do that..")
+            positionlogg.logtex.enqueue("Im sorry Dave,im afraid i cant do that..")
         else:
             if PosX<112/4:
                 H.step(30,0.01,False)
-                print("Stepping Right")
+                positionlogg.logtex.enqueue("Stepping Right")
             else:
                 H.step(30,0.01,True)
-                print("Stepping Left")
+                positionlogg.logtex.enqueue("Stepping Left")
                 
 def image_module(positionlogg):
     """Image module that takes care of taking images with the camera and processing it"""
+    positionlogg.logtex.enqueue('Initializing image module')
     camera=picamera.PiCamera()
     implementsettings(camera)
     while True:
         image=takeRGBimage(camera).array
         currentPos=positionlogg.COV #So we know where the image was taken
+        positionlogg.logtex.enqueue('Taking picture at' + str(currentPos))
         im2=image.copy()
         FiltIm=GreenFilt(im2,[100,210,100],10)
+        positionlogg.logtex.enqueue('Filtering')
         #misc.imsave('TestPic' +str(n)+'.jpeg', image)
         #misc.imsave('TestPicGreen' +str(n)+'.jpeg', FiltIm)
         [PosX,PosY]=GreenPos(FiltIm)
+        positionlogg.logtex.enqueue('Position found')
         positionlogg.errorvalue=PosX
 
 ###################################################################
     
 if __name__=="__main__":
-    print('Setting up logg')
+    print('Setting up log')
     pl=positionlogg()
     print('Setting up modules')
     init_threaded_modules()
     while True:
-        time.sleep(1000)
+        if pl.logtext:
+            print(pl.logtext.dequeue())
     
