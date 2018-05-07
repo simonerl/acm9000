3b02891fdb7e78494b36d533b1aa130da66f9022
simonerl <simonerl@kth.se>
2018-05-07 16:05:25 +0200

    invertera PosX

--- Moduler/imagetest.py
+++ Moduler/imagetest.py
@@ -47,7 +47,7 @@ while True:
         print('WITHIN 10 PIXELS. NO NEW POSITION ASSIGNED')
     t6=time.time()
     #Saving images:
-    FiltIm[:,int(PosX)]=127
+    FiltIm[:,int(112-PosX)]=127 #PosX är inverterad i jämförelse till bilden!!!
     misc.imsave('/media/pi/USB DISK/TESTBILDER/testi'+str(num)+'.jpg', image)
     misc.imsave('/media/pi/USB DISK/TESTBILDER/testg'+str(num)+'.jpg', FiltIm)
     num+=1
